be3a004901140ed7b0c13e5ede38b8936c02789f
Lupesms97 <lupesms97@gmail.com>
2024-05-05 20:18:40 -0300

    dev: get data for the method

--- src/main/java/org/processor/cnab/infrastructure/config/BatchConfig.java
+++ src/main/java/org/processor/cnab/infrastructure/config/BatchConfig.java
@@ -5,8 +5,11 @@ import org.processor.cnab.domain.entity.TransactionEntity;
 import org.processor.cnab.presentation.mapper.FromDto;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
@@ -17,9 +20,11 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.Range;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -56,15 +61,16 @@ public class BatchConfig {
                 .processor(processor)
                 .writer(writer)
                 .build();
-
-
     }
 
+    @StepScope
     @Bean
-    FlatFileItemReader<TransactionCNAB> reader() {
+    FlatFileItemReader<TransactionCNAB> reader(
+            @Value("#{jobParameters['cnabFile']}" )Resource resource
+    ) {
         return new FlatFileItemReaderBuilder<TransactionCNAB>()
                 .name("reader")
-                .resource(new FileSystemResource("files/CNAB.txt"))
+                .resource(resource)
                 .fixedLength()
                 .columns(
                         new Range(1, 1),
@@ -100,6 +106,18 @@ public class BatchConfig {
                         "VALUES (:type, :date, :amount, :cpf, :card, :hour, :storeOwner, :storeName)")
                 .beanMapped()
                 .build();
+    }
+
+    @Bean
+    JobLauncher jobLauncherAsync(JobRepository jobRepository) throws Exception {
+        var jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+
+
+
     }
 
 
