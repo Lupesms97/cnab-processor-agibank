aa46dbaeb017162ff2d1c098a8f8ca21c5cf9364
Lupesms97 <lupesms97@gmail.com>
2024-05-05 20:21:58 -0300

    dev: change the return type end how the data come to the controller

---
+++ src/main/java/org/processor/cnab/application/service/ProcessorServicer.java
@@ -0,0 +1,58 @@
+package org.processor.cnab.application.service;
+
+
+
+import org.processor.cnab.presentation.exception.CouldNotStoreException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class ProcessorServicer {
+
+    private final Path fileStorageLocation;
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    public ProcessorServicer(
+            @Value("${file.upload.dir}")String fileUploadDir,
+            final Job job,
+            @Qualifier("jobLauncherAsync") final JobLauncher jobLauncher) {
+        this.fileStorageLocation = Paths.get(fileUploadDir);
+        this.job = job;
+        this.jobLauncher = jobLauncher;
+    }
+
+
+    public void uploadFile(MultipartFile file) throws Exception {
+        var fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        var targetLocation = this.fileStorageLocation.resolve(fileName);
+
+        file.transferTo(targetLocation);
+
+
+        var jobParameters = new JobParametersBuilder()
+                .addJobParameter("cnab",
+                        file.getOriginalFilename(),
+                        String.class,
+                        true)
+                .addJobParameter("cnabFile",
+                        "file:"+targetLocation.toString(),
+                        String.class, false)
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters) ;
+        JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+
+    }
+}
---
+++ src/main/java/org/processor/cnab/presentation/controller/ProcessController.java
@@ -0,0 +1,30 @@
+package org.processor.cnab.presentation.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.processor.cnab.application.service.ProcessorServicer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/v1/process")
+@RequiredArgsConstructor
+public class ProcessController {
+
+    private final ProcessorServicer processorServicer;
+
+    @PostMapping("/cnab")
+    public ResponseEntity<String> processCNAB(@RequestParam("file") MultipartFile file) throws Exception {
+        processorServicer.uploadFile(file);
+        return ResponseEntity.status(HttpStatus.CREATED).body("File processed successfully.");
+    }
+
+
+
+}
